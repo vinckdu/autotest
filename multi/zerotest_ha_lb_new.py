b5f1340c32d755757d9232f4d7432353c1061939
root <root@sg0001.asia-southeast1-b.c.hypnotic-beaker-266808.internal>
2020-11-07 14:32:27 +0000

    first release on develop branch

---
+++ multi/zerotest_ha_lb_new.py
@@ -0,0 +1,51 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*- 
+"""
+@author:Vinck.Du
+@file: insert.py
+@time: 2019/09/15
+"""
+import boto3
+import botocore
+import time
+import threading
+import random
+import sys
+import ssl
+import commands
+from datetime import datetime
+try:
+    ssl._create_default_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+except Exception as exc:
+    print(exc)
+
+access_key = 'NZF839DRD334XA15A36A'
+secret_key = 'jdxX6JDXOgqXCUXRhtAv8DyRbGBJ2WawsOUqlEH5'
+
+def putobject(Bucket,Key,Body):
+    dt=datetime.now()
+    mytime = dt.strftime( '%Y-%m-%d %H:%M:%S %f' )
+    print(mytime +"  put to 192.168.242.16")
+    s3 = boto3.resource(
+    service_name ='s3',
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_key,
+    endpoint_url = 'http://192.168.242.16:8082',
+    verify = False
+    )
+
+    try:
+        s3.Bucket(Bucket).put_object(Key=Key,Body=Body)
+    except Exception as e:
+        print("There is error when uploading")
+        print(e)
+
+
+myint = int(sys.argv[3])
+for i in range(1,myint) :
+    putobject(sys.argv[1],sys.argv[2]+"-"+str(i),"hello,world!"*10)
+    print("Now objects "+sys.argv[1]+" "+ sys.argv[2]+"-"+str(i) +" created !")
+
+
